8b1a65811c45547aa75d8fec6046987a9e95084a
Cooper Aerial Surveys <cooperaerial@users.noreply.github.com>
2018-02-27 14:34:33 -0700

    Add owner to reg

--- runafter.ps1
+++ runafter.ps1
@@ -5,5 +5,15 @@ Write-Host "Enabling display and sleep mode timeouts..."
 powercfg /X monitor-timeout-ac 30
 powercfg /X monitor-timeout-dc 5
 
+Write-Host "Set RegisteredOrganization and RegisteredOwner..."
+If (!(Test-Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOrganization")) {
+    New-Item -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOrganization" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "RegisteredOrganization" -Type String -Value "Cooper Aerial Surveys Co."
+If (!(Test-Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOwner")) {
+    New-Item -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\RegisteredOwner" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name "RegisteredOwner" -Type String -Value "Cooper Aerial"
+
 Write-Host "Restarting..."
 Restart-Computer
