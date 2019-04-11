acc1f2d36026629427b2f85012586d454f85bf4a
Cooper Aerial Surveys <cooperaerial@users.noreply.github.com>
2019-04-11 12:13:24 -0700

    Updates for new user

--- NewUser.bat
+++ NewUser.bat
@@ -1,3 +1,4 @@
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "NewUser.txt"
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "RemoveBloat.txt"
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0MapDrives.ps1"
 pause
