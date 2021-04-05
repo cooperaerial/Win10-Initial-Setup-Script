ec2d5505a605a0e4c62b03adb13ed1ffd7ab27e9
Jeremy Brockman <jeremy@cooperaerial.com>
2021-04-05 09:43:59 -0700

    Fix tipo

--- UserTweeks.ps1
+++ UserTweeks.ps1
@@ -1,5 +1,5 @@
 Write-Host "Show taskbar buttons on taskbar where windows is open..."
-If (!(Test-Path "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced")) {
-  New-Item -Path "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Force | Out-Null
+If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced")) {
+  New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Force | Out-Null
 }
-Set-ItemProperty -Path "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "MMTaskbarMode" -Type DWord -Value 2
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "MMTaskbarMode" -Type DWord -Value 2
