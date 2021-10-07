1fa47b6f7155ea398401e0d88f0783dc39c4b535
Jeremy Brockman <jeremy@cooperaerial.com>
2021-10-07 12:54:55 -0700

    Fix Remove News and Interests from Taskbar

--- UserTweeks.ps1
+++ UserTweeks.ps1
@@ -5,8 +5,8 @@ If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanc
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "MMTaskbarMode" -Type DWord -Value 2
 
 Write-Output "Remove News and Interests from Taskbar..."
-If (!(Test-Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Feeds")) {
-  New-Item -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Feeds" -Force | Out-Null
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds")) {
+  New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds" -Force | Out-Null
 }
-Set-ItemProperty -Path "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Type DWord -Value 2
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Type DWord -Value 2
 
