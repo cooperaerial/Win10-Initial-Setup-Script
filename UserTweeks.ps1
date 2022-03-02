23b1b35bcacae8dd49ee259f8b98bf597195d133
Jeremy Brockman <jeremy@cooperaerial.com>
2022-03-02 13:34:55 -0700

    Remove Windows 11 Chat and Widgets taskbar buttons

--- UserTweeks.ps1
+++ UserTweeks.ps1
@@ -4,6 +4,12 @@ If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanc
 }
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "MMTaskbarMode" -Type DWord -Value 2
 
+Write-Output "Hide windows 11 Chat Button..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Type DWord -Value 0
+
+Write-Output "Hide windows 11 Widgets Button..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Type DWord -Value 0
+
 Write-Output "Remove News and Interests from Taskbar..."
 If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds")) {
   New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds" -Force | Out-Null
