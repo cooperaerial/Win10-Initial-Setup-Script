dc413ca4f5b20112d8d65937a1f2adc734ba4ef7
Jeremy Brockman <jeremy@cooperaerial.com>
2022-08-10 11:21:24 -0700

    Remove F: drive

--- MapDrives.ps1
+++ MapDrives.ps1
@@ -1,7 +1,3 @@
-
-Write-Host "Map FTPRoot as F: ..."
-New-PSDrive -Name "F" -Root "\\phx-storage\FTPRoot" -Persist -PSProvider "FileSystem"
-
 Write-Host "Map Lidar as L: ..."
 New-PSDrive -Name "L" -Root "\\phx-storage\Lidar" -Persist -PSProvider "FileSystem"
 
@@ -10,6 +6,7 @@ New-PSDrive -Name "M" -Root "\\phx-storage\Management" -Persist -PSProvider "Fil
 
 Write-Host "Map ToArchive as T: ..."
 New-PSDrive -Name "T" -Root "\\phx-storage\ToArchive" -Persist -PSProvider "FileSystem"
+
 Write-Host "Map ToArchive as T: ..."
 New-PSDrive -Name "X" -Root "\\tuc-jobs\projects" -Persist -PSProvider "FileSystem"
 Net Use
