4ef303ac26a345f755fc1703e20f7cfa38bc4d9e
Jeremy Brockman <jeremy@cooperaerial.com>
2022-08-10 11:16:28 -0700

    Add winget

--- getinstalledapps.ps1
+++ getinstalledapps.ps1
@@ -1,1 +1,2 @@
 Start-Process powershell -Verb runas "Get-AppxPackage -AllUsers | Select Name, InstallLocation > c:\AppxList.txt"
+Start-Process powershell -Verb runas "winget list > c:\WingetList.txt"
