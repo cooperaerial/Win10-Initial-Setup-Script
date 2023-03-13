fa36a9e4f85bf9b141581356b355ff2435758e52
Jeremy Brockman <jeremy@cooperaerial.com>
2023-03-13 08:48:22 -0700

    remove pause

--- NewUser.bat
+++ NewUser.bat
@@ -2,4 +2,3 @@
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0UserTweeks.ps1"
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "NewUser.txt"
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "RemoveBloat.txt"
-pause
